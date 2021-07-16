70ae7be1372abe2a41eb203bee6e994ade6c20b5
Tracy Ma <1991md@gmail.com>
2021-07-16 13:34:52 +0800

    Bump version to 1.0.0.22, 同时关闭文件日志功能

--- debug.h
+++ debug.h
@@ -12,17 +12,17 @@ namespace lxd {
 	DLL_PUBLIC void print(char const* format_str, Args&&... args) {
 		std::string output = fmt::format(format_str, std::forward<Args>(args)...);
 		OutputDebugStringA(output.c_str());
-		DWORD offsetHigh;
-		AsyncFile::GetInstance()->Write(output.c_str(), output.size(), AsyncFile::GetInstance()->GetFileLength(&offsetHigh), 0);
+		//DWORD offsetHigh;
+		//AsyncFile::GetInstance()->Write(output.c_str(), output.size(), AsyncFile::GetInstance()->GetFileLength(&offsetHigh), 0);
 	}
 
 	template <typename... Args>
 	DLL_PUBLIC void print(wchar_t const* format_str, Args&&... args) {
 		std::wstring output = fmt::format(format_str, std::forward<Args>(args)...);
 		OutputDebugStringW(output.c_str());
-		auto utf8 = utf8_encode(output);
-		DWORD offsetHigh;
-		AsyncFile::GetInstance()->Write(utf8.c_str(), utf8.size(), AsyncFile::GetInstance()->GetFileLength(&offsetHigh), 0);
+		//auto utf8 = utf8_encode(output);
+		//DWORD offsetHigh;
+		//AsyncFile::GetInstance()->Write(utf8.c_str(), utf8.size(), AsyncFile::GetInstance()->GetFileLength(&offsetHigh), 0);
 	}
 
 	#define verify(x) {if(!(x)) _asm{int 0x03}}
