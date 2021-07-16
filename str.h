99da75a46c950af1f999cc6f20bc3e1b979302e2
Tracy Ma <1991md@gmail.com>
2021-07-16 13:34:10 +0800

    string: new 增加大小写转换方法; 同时 string.h/cpp 被重命名为 str.h/cpp 避免和系统 <string.h/cpp> 冲突

--- str.h
+++ str.h
@@ -5,5 +5,9 @@
 #include <string_view>
 
 namespace lxd {
+	DLL_PUBLIC void Upper(std::string& str);
+	DLL_PUBLIC void Upper(std::wstring& str);
+	DLL_PUBLIC void Lower(std::string& str);
+	DLL_PUBLIC void Lower(std::wstring& str);
 	DLL_PUBLIC std::vector<std::string_view> Split(std::string_view src, std::string_view separate_character);
 }
