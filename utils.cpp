1aebd9b334be7e3706c8ce7ca7fb89336f9fe003
马迪 <di.ma@bonsmile.com>
2021-07-30 17:18:22 +0800

    Uitls: new 实现设置和获取用户环境变量的接口

--- utils.cpp
+++ utils.cpp
@@ -43,4 +43,64 @@ namespace lxd {
 
         return result;
     }
+
+    void SetEnv(std::wstring_view name, int value) {
+        HKEY subKey;
+        int _value = value;
+        RegOpenKeyEx(HKEY_CURRENT_USER, L"Environment", 0, KEY_SET_VALUE, &subKey);
+        RegSetValueEx(subKey,
+                      name.data(),
+                      0,
+                      REG_DWORD,
+                      reinterpret_cast<const BYTE*>(&_value),
+                      sizeof(int));
+        RegCloseKey(HKEY_CURRENT_USER);
+    }
+
+    void SetEnv(std::wstring_view name, std::wstring_view value) {
+        HKEY subKey;
+        RegOpenKeyEx(HKEY_CURRENT_USER, L"Environment", 0, KEY_SET_VALUE, &subKey);
+        auto status = RegSetValueEx(subKey,
+                                    name.data(),
+                                    0,
+                                    REG_SZ,
+                                    reinterpret_cast<const BYTE*>(value.data()),
+                                    value.size() * sizeof(wchar_t));
+        assert(status == ERROR_SUCCESS);
+        RegCloseKey(HKEY_CURRENT_USER);
+    }
+
+    std::variant<int, std::wstring> GetEnv(std::wstring_view name) {
+        HKEY subKey;
+        DWORD size;
+        DWORD type;
+        std::variant<int, std::wstring> result;
+        RegOpenKeyEx(HKEY_CURRENT_USER, L"Environment", 0, KEY_QUERY_VALUE, &subKey);
+        auto status = RegQueryValueEx(subKey, name.data(), 0, NULL, NULL, &size);
+        if(ERROR_SUCCESS != status) {
+
+        } else {
+            // This environment variable already exists. Assign its value to this
+            // EnvVar object.
+            auto data = new BYTE[size];
+            RegQueryValueEx(subKey, name.data(), 0, &type, data, &size);
+            switch(type) {
+                case REG_DWORD:
+                    result = *reinterpret_cast<int*>(data);
+                    break;
+                case REG_SZ:
+                {
+                    std::wstring value = reinterpret_cast<wchar_t*>(data);
+                    result = value;
+                }
+                break;
+                default:
+                    break;
+            }
+            delete[] data;
+        }
+        RegCloseKey(HKEY_CURRENT_USER);
+
+        return result;
+    }
 }
--- utils.h
+++ utils.h
@@ -2,10 +2,14 @@
 
 #include "defines.h"
 #include <string>
+#include <variant>
 
 namespace lxd {
 	DLL_PUBLIC std::wstring GetPathOfExe();
 	DLL_PUBLIC std::wstring GetDirOfExe();
 	DLL_PUBLIC std::wstring GetExeName();
 	DLL_PUBLIC std::wstring GetPathOfAppData();
+	DLL_PUBLIC void SetEnv(std::wstring_view name, int value);
+	DLL_PUBLIC void SetEnv(std::wstring_view name, std::wstring_view value);
+	DLL_PUBLIC std::variant<int, std::wstring> GetEnv(std::wstring_view name);
 }
