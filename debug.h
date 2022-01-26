74b11ee87af7135d87be2331b1bc8011b51ccb4e
马迪 <di.ma@bonsmile.com>
2022-01-26 15:54:46 +0800

    opt: 精简 lxd::print 实现

--- debug.h
+++ debug.h
@@ -1,32 +1,23 @@
 #pragma once
 
-//#include <fmt/xchar.h>
 #include <fmt/ostream.h>
+#include <fmt/xchar.h>
 #include <Windows.h>
 #include <debugapi.h>
-//#if defined(_DEBUG)
-#include "AsyncFile.h"
-#include "encoding.h"
-#include <fmt/xchar.h>
-#include <fmt/format.h>
+#include "defines.h"
 
 //#endif
 namespace lxd {
 	template <typename... Args>
-	DLL_PUBLIC void print(const fmt::format_string<Args...>& format_str, Args&&... args) {
-		std::string output = fmt::format(format_str, std::forward<Args>(args)...);
+	DLL_PUBLIC void print(fmt::string_view fmt, Args&&... args) {
+		std::string output = fmt::vformat(fmt, fmt::make_format_args(std::forward<Args>(args)...));
 		OutputDebugStringA(output.c_str());
-		//DWORD offsetHigh;
-		//AsyncFile::GetInstance()->Write(output.c_str(), output.size(), AsyncFile::GetInstance()->GetFileLength(&offsetHigh), 0);
 	}
 
 	template <typename... Args>
-	DLL_PUBLIC void print(const wchar_t* format_str, Args&&... args) {
-		std::wstring output = fmt::format(format_str, std::forward<Args>(args)...);
+	DLL_PUBLIC void print(fmt::wstring_view fmt, Args&&... args) {
+		std::wstring output = fmt::vformat(fmt, fmt::make_wformat_args(std::forward<Args>(args)...));
 		OutputDebugStringW(output.c_str());
-		//auto utf8 = utf8_encode(output);
-		//DWORD offsetHigh;
-		//AsyncFile::GetInstance()->Write(utf8.c_str(), utf8.size(), AsyncFile::GetInstance()->GetFileLength(&offsetHigh), 0);
 	}
 
 	#define verify(x) {if(!(x)) _asm{int 0x03}}
