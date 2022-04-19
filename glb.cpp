1bb75cc39333943eae72e7bbb87ff1ac61475efd
馬迪 <di.ma@bonsmile.com>
2022-04-19 15:18:20 +0800

    new: 从 STL 生成 glb（2/5）
    导出成功，但是缺少 accessor.min 和 accessor.max

--- glb.cpp
+++ glb.cpp
@@ -3,170 +3,177 @@
 #include "json.h"
 #include <unordered_map>
 #include "debug.h"
+#include "fileio.h"
 
-struct Vec3 {
-	float v[3];
-	friend bool operator==(const Vec3& a, const Vec3& b) {
-		return a.v[0] == b.v[0] && a.v[1] == b.v[1] && a.v[2] == b.v[2];
+namespace lxd {
+	template <class T>
+	bool Glb<T>::load(std::string_view buffer) {
+		assert(buffer.size() > sizeof(Header));
+
+		auto magic = buffer.substr(0, 4);
+		if(magic != "glTF")
+			return false;
+		auto pLength = reinterpret_cast<const uint32_t*>(buffer.data() + offsetof(Header, length));
+		if(*pLength != buffer.size())
+			return false;
+
+		extractChunk(buffer.data() + sizeof(Header), buffer.size() - sizeof(Header));
+
+		return true;
 	}
-};
 
-namespace std {
-	template <>
-	struct hash<Vec3> {
-		size_t operator()(const Vec3& vec) const {
-			size_t seed = 0;
-			for(int i = 0; i < 3; ++i) {
-				seed ^=
-					std::hash<float>()(vec.v[i]) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-			}
-			return seed;
-		}
-	};
-}
-
-bool lxd::Glb::load(std::string_view buffer) {
-	assert(buffer.size() > sizeof(Header));
-
-	auto magic = buffer.substr(0, 4);
-	if(magic != "glTF")
-		return false;
-	auto pLength = reinterpret_cast<const uint32_t*>(buffer.data() + offsetof(Header, length));
-	if(*pLength != buffer.size())
-		return false;
-
-	extractChunk(buffer.data() + sizeof(Header), buffer.size() - sizeof(Header));
-
-	return true;
-}
-
-bool lxd::Glb::loadFromStl(std::string_view buffer) {
-#pragma pack(1)
-	struct StlFacet {
-		float faceNormal[3];
-		float v1[3];
-		float v2[3];
-		float v3[3];
-		unsigned short attribute;
-	};
-#pragma pack()
-	// *.stl format:
-	//UINT8[80]    – Header - 80 bytes
-	//UINT32       – Number of triangles - 4 bytes
-	auto nFacet = *reinterpret_cast<const uint32_t*>(buffer.data() + 80);
-	std::unordered_map<Vec3, int> mapVtxId;
-	std::unordered_map<Vec3, int>::iterator iter;
-	mapVtxId.reserve(nFacet);
-	std::vector<Vec3> points; // 顶点
-	std::vector<uint16_t> indices; // 索引
-	int vId = 0;
-	for(uint32_t i = 0; i < nFacet; i++) {
-		auto pFacet = reinterpret_cast<const StlFacet*>(buffer.data() + 84 + i * sizeof(StlFacet));
-		auto pV1 = pFacet->v1;
-		for(int j = 0; j < 3; j++) {
-			const Vec3* pVec3 = reinterpret_cast<const Vec3*>(pV1 + j * 3);
-			iter = mapVtxId.find(*pVec3);
-			if(iter == mapVtxId.end()) {
-				points.push_back(*pVec3);
-				mapVtxId.insert(std::make_pair(*pVec3, vId));
-				indices.push_back(static_cast<uint16_t>(vId));
-				vId++;
-			} else {
-				indices.push_back(static_cast<uint16_t>(iter->second));
-			}
-		}
-	}
-	// JSON
-	char* json = NULL;
-	int length = 0;
-	{
-		// asset
-		ksJson* rootNode = ksJson_SetObject(ksJson_Create());
-		ksJson* asset = ksJson_SetObject(ksJson_AddObjectMember(rootNode, "asset"));
-		ksJson_SetFloat(ksJson_AddObjectMember(asset, "version"), 2.0);
-		// buffers
+	template <class T>
+	bool Glb<T>::load(const std::vector<MyVec3>& points, const std::vector<T>& indices) {
+		clear();
+		// Binary Buffer
+		Chunk chunk;
+		chunk.type = 0x004E4942; // BIN
 		{
-			ksJson* buffers = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "buffers"));
-			ksJson* posBuffer = ksJson_SetObject(ksJson_AddArrayElement(buffers));
-			ksJson_SetUint32(ksJson_AddObjectMember(posBuffer, "byteLength"), points.size() * sizeof(Vec3));
-			ksJson* idxBuffer = ksJson_SetObject(ksJson_AddArrayElement(buffers));
-			ksJson_SetUint32(ksJson_AddObjectMember(idxBuffer, "byteLength"), indices.size() * sizeof(uint16_t));
+			auto pIndices = reinterpret_cast<const char*>(indices.data());
+			chunk.data.assign(pIndices, pIndices + sizeof(T) * indices.size());
+			BufferView bufferView{.buffer = 0, .byteOffset = 0, .byteLength = static_cast<int>(chunk.data.size()), .target = 34963};
+			m_bufferViews.push_back(bufferView);
 		}
-		// buffer views
 		{
+			BufferView bufferView{.buffer = 0, .byteOffset = static_cast<int>(chunk.data.size()), .target = 34962};
+			auto pPoints = reinterpret_cast<const char*>(points.data());
+			chunk.data.insert(chunk.data.end(), pPoints, pPoints + sizeof(MyVec3) * points.size());
+			// 每个 Chunk 末尾需要 4 字节对齐
+			auto curSize = chunk.data.size();
+			if(curSize % 4 != 0) {
+				size_t extra = 4 - curSize % 4;
+				for(size_t i = 0; i < extra; i++) {
+					chunk.data.push_back(0x00);
+				}
+			}
+			bufferView.byteLength = static_cast<int>(chunk.data.size() - bufferView.byteOffset);
+			m_bufferViews.push_back(bufferView);
+		}
+		// JSON
+		char* json = NULL;
+		int length = 0;
+		{
+			// asset
+			ksJson* rootNode = ksJson_SetObject(ksJson_Create());
+			ksJson* asset = ksJson_SetObject(ksJson_AddObjectMember(rootNode, "asset"));
+			ksJson_SetString(ksJson_AddObjectMember(asset, "version"), "2.0");
+			// buffers
+			{
+				ksJson* buffers = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "buffers"));
+				ksJson* idxBuffer = ksJson_SetObject(ksJson_AddArrayElement(buffers));
+				ksJson_SetUint64(ksJson_AddObjectMember(idxBuffer, "byteLength"), chunk.data.size());
+			}
+			// buffer views
 			ksJson* bufferViews = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "bufferViews"));
-			ksJson* posBufferView = ksJson_SetObject(ksJson_AddArrayElement(bufferViews));
-			ksJson_SetUint32(ksJson_AddObjectMember(posBufferView, "buffer"), 0);
-			ksJson_SetUint32(ksJson_AddObjectMember(posBufferView, "byteOffset"), 0);
-			ksJson_SetUint32(ksJson_AddObjectMember(posBufferView, "byteLength"), points.size() * sizeof(Vec3));
-			ksJson_SetUint32(ksJson_AddObjectMember(posBufferView, "target"), 34963); // ELEMENT_ARRAY_BUFFER
-			ksJson* idxBufferView = ksJson_SetObject(ksJson_AddArrayElement(bufferViews));
-			ksJson_SetUint32(ksJson_AddObjectMember(idxBufferView, "buffer"), 1);
-			ksJson_SetUint32(ksJson_AddObjectMember(idxBufferView, "byteOffset"), 0);
-			ksJson_SetUint32(ksJson_AddObjectMember(idxBufferView, "byteLength"), points.size() * sizeof(Vec3));
+			for(auto& bufferView : m_bufferViews) {
+				ksJson* pBufferView = ksJson_SetObject(ksJson_AddArrayElement(bufferViews));
+				ksJson_SetUint32(ksJson_AddObjectMember(pBufferView, "buffer"), bufferView.buffer);
+				ksJson_SetUint32(ksJson_AddObjectMember(pBufferView, "byteOffset"), bufferView.byteOffset);
+				ksJson_SetUint64(ksJson_AddObjectMember(pBufferView, "byteLength"), bufferView.byteLength);
+				ksJson_SetUint32(ksJson_AddObjectMember(pBufferView, "target"), bufferView.target);
+			}
+			// accessors
+			{
+				ksJson* accessors = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "accessors"));
+				ksJson* idxAccessor = ksJson_SetObject(ksJson_AddArrayElement(accessors));
+				ksJson_SetUint32(ksJson_AddObjectMember(idxAccessor, "bufferView"), 0);
+				ksJson_SetUint32(ksJson_AddObjectMember(idxAccessor, "byteOffset"), 0);
+				ksJson_SetUint32(ksJson_AddObjectMember(idxAccessor, "componentType"), sizeof(T) == 2 ?  5123 : 5125); // unsigned short
+				ksJson_SetUint64(ksJson_AddObjectMember(idxAccessor, "count"), indices.size());
+				ksJson_SetString(ksJson_AddObjectMember(idxAccessor, "type"), "SCALAR");
+				ksJson* posAccessor = ksJson_SetObject(ksJson_AddArrayElement(accessors));
+				ksJson_SetUint32(ksJson_AddObjectMember(posAccessor, "bufferView"), 1);
+				ksJson_SetUint32(ksJson_AddObjectMember(posAccessor, "byteOffset"), 0);
+				ksJson_SetUint32(ksJson_AddObjectMember(posAccessor, "componentType"), 5126);// float
+				ksJson_SetUint64(ksJson_AddObjectMember(posAccessor, "count"), points.size());
+				ksJson_SetString(ksJson_AddObjectMember(posAccessor, "type"), "VEC3");
+			}
+			// meshes
+			{
+				ksJson* meshes = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "meshes"));
+				ksJson* mesh0 = ksJson_SetObject(ksJson_AddArrayElement(meshes));
+				ksJson* primitives = ksJson_SetArray(ksJson_AddObjectMember(mesh0, "primitives"));
+				ksJson* primitive0 = ksJson_SetObject(ksJson_AddArrayElement(primitives));
+				ksJson* attributes = ksJson_SetObject(ksJson_AddObjectMember(primitive0, "attributes"));
+				ksJson_SetUint32(ksJson_AddObjectMember(primitive0, "indices"), 0); // accessor 0
+				ksJson_SetUint32(ksJson_AddObjectMember(attributes, "POSITION"), 1); // accessor 1
+			}
+			// nodes
+			{
+				ksJson* nodes = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "nodes"));
+				ksJson* node0 = ksJson_SetObject(ksJson_AddArrayElement(nodes));
+				ksJson_SetUint32(ksJson_AddObjectMember(node0, "mesh"), 0);
+			}
+			// scene
+			{
+				ksJson_SetUint32(ksJson_AddObjectMember(rootNode, "scene"), 0);
+				ksJson* scenes = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "scenes"));
+				ksJson* scene0 = ksJson_SetObject(ksJson_AddArrayElement(scenes));
+				ksJson* nodes = ksJson_SetArray(ksJson_AddObjectMember(scene0, "nodes"));
+				ksJson_SetUint32(ksJson_AddArrayElement(nodes), 0);
+			}
+			ksJson_WriteToBuffer(rootNode, &json, &length);
+			ksJson_Destroy(rootNode);
 		}
-		// accessors
+		lxd::print("{}\n", json);
+		// JSON
+		Chunk jsonChunk;
+		jsonChunk.type = 0x4E4F534A; // JSON
 		{
-			ksJson* accessors = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "accessors"));
-			ksJson* posAccessor = ksJson_SetObject(ksJson_AddArrayElement(accessors));
-			ksJson_SetUint32(ksJson_AddObjectMember(posAccessor, "bufferView"), 0);
-			ksJson_SetUint32(ksJson_AddObjectMember(posAccessor, "byteOffset"), 0);
-			ksJson_SetUint32(ksJson_AddObjectMember(posAccessor, "componentType"), 5126);// float
-			ksJson_SetUint32(ksJson_AddObjectMember(posAccessor, "count"), points.size());
-			ksJson_SetString(ksJson_AddObjectMember(posAccessor, "type"), "VEC3");
-			ksJson* idxAccessor = ksJson_SetObject(ksJson_AddArrayElement(accessors));
-			ksJson_SetUint32(ksJson_AddObjectMember(idxAccessor, "bufferView"), 1);
-			ksJson_SetUint32(ksJson_AddObjectMember(idxAccessor, "byteOffset"), 0);
-			ksJson_SetUint32(ksJson_AddObjectMember(idxAccessor, "componentType"), 5123); // unsigned short
-			ksJson_SetUint32(ksJson_AddObjectMember(idxAccessor, "count"), indices.size());
-			ksJson_SetString(ksJson_AddObjectMember(idxAccessor, "type"), "SCALAR");
+			jsonChunk.data.assign(json, json + length);
+			// 每个 Chunk 末尾需要 4 字节对齐
+			auto curSize = jsonChunk.data.size();
+			if(curSize % 4 != 0) {
+				auto extra = 4 - curSize % 4;
+				for(size_t n = 0; n < extra; n++) {
+					jsonChunk.data.push_back(' '); // JSON 利用空格字符对齐
+				}
+			}
 		}
-		// meshes
-		{
-			ksJson* meshes = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "meshes"));
-			ksJson* mesh0 = ksJson_SetObject(ksJson_AddArrayElement(meshes));
-			ksJson* primitives = ksJson_SetArray(ksJson_AddObjectMember(mesh0, "primitives"));
-			ksJson* primitive0 = ksJson_SetObject(ksJson_AddArrayElement(primitives));
-			ksJson* attributes = ksJson_AddObjectMember(primitive0, "attributes");
-			ksJson_SetUint32(ksJson_AddObjectMember(attributes, "POSITION"), 0); // accessor 0
-			ksJson_SetUint32(ksJson_AddObjectMember(primitive0, "indices"), 1); // accessor 1
-		}
-		// nodes
-		{
-			ksJson* nodes = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "nodes"));
-			ksJson* node0 = ksJson_SetObject(ksJson_AddArrayElement(nodes));
-			ksJson_SetUint32(ksJson_AddObjectMember(node0, "mesh"), 0);
-		}
-		// scene
-		{
-			ksJson_SetUint32(ksJson_AddObjectMember(rootNode, "scene"), 0);
-			ksJson* scenes = ksJson_SetArray(ksJson_AddObjectMember(rootNode, "scenes"));
-			ksJson* scene0 = ksJson_SetObject(ksJson_AddArrayElement(scenes));
-			ksJson* nodes = ksJson_SetArray(ksJson_AddObjectMember(scene0, "nodes"));
-			ksJson_SetUint32(ksJson_AddArrayElement(nodes), 0) ;
-		}
-		ksJson_WriteToBuffer(rootNode, &json, &length);
-		ksJson_Destroy(rootNode);
+		free(json);
+		m_chunks.emplace_back(std::move(jsonChunk));
+		m_chunks.emplace_back(std::move(chunk));
+		return true;
 	}
-	lxd::print("{}\n", json);
-	return true;
-}
 
-void createJson() {
-	
-}
+	template <class T>
+	bool Glb<T>::save(const std::wstring& path) {
+		lxd::File file(path, lxd::WriteOnly | lxd::Truncate);
+		m_header.length = static_cast<uint32_t>(sizeof(Header));
+		for(const auto& chunk : m_chunks) {
+			m_header.length += static_cast<uint32_t>(2 * sizeof(uint32_t) + chunk.data.size());
+		}
+		bool result = true;
+		result &= file.write(&m_header, sizeof(Header));
+		for(const auto& chunk : m_chunks) {
+			uint32_t length = static_cast<uint32_t>(chunk.data.size());
+			uint32_t type = chunk.type;
+			result &= file.write(&length, sizeof(length));
+			result &= file.write(&type, sizeof(type));
+			result &= file.write(chunk.data.data(), chunk.data.size());
+		}
+		assert(file.size() == m_header.length);
+		return result;
+	}
 
-bool lxd::Glb::save(std::wstring_view path) {
+	template <class T>
+	void Glb<T>::clear() {
+		m_chunks.clear();
+		m_header.length = 0;
+	}
 
-	return false;
-}
+	template <class T>
+	void Glb<T>::extractChunk(const char* data, size_t size) {
+		Chunk chunk;
+		uint32_t length = *reinterpret_cast<const uint32_t*>(data);
+		chunk.type = *reinterpret_cast<const uint32_t*>(data + 4);
+		chunk.data.assign(const_cast<char*>(data) + 4 + 4, const_cast<char*>(data) + 4 + 4 + length);
+		m_chunks.emplace_back(std::move(chunk));
+		if(4 + 4 + length < size)
+			extractChunk(data + 4 + 4 + length, size - 4 - 4 - length);
+	}
 
-void lxd::Glb::extractChunk(const char* data, size_t size) {
-	Chunk chunk;
-	uint32_t length = *reinterpret_cast<const uint32_t*>(data);
-	chunk.type = *reinterpret_cast<const uint32_t*>(data + 4);
-	chunk.data.assign(const_cast<char*>(data)+4+4, const_cast<char*>(data)+4+4+length);
-	m_chunks.emplace_back(std::move(chunk));
-	if(4 + 4 + length < size)
-		extractChunk(data + 4 + 4 + length, size - 4 - 4 - length);
+	// 顶点索引暂时只支持 uint16/uint32
+	template class Glb<uint16_t>;
+	template class Glb<uint32_t>;
 }
--- glb.h
+++ glb.h
@@ -6,6 +6,14 @@
 #include <string_view>
 
 namespace lxd {
+	struct MyVec3 {
+		float v[3];
+		friend bool operator==(const MyVec3& a, const MyVec3& b) {
+			return a.v[0] == b.v[0] && a.v[1] == b.v[1] && a.v[2] == b.v[2];
+		}
+	};
+
+	template <class T>
 	class DLL_PUBLIC Glb {
 	public:
 		struct Header {
@@ -17,13 +25,28 @@ namespace lxd {
 			uint32_t type;
 			std::vector<char> data;
 		};
+		struct BufferView {
+			int buffer;
+			int byteOffset;
+			int byteLength;
+			int byteStride;
+			int target;
+		};
+		Glb() {
+			m_header.magic = 0x46546C67;
+			m_header.version = 2;
+			m_header.length = 0;
+		}
+		~Glb() {}
 		bool load(std::string_view buffer);
-		bool loadFromStl(std::string_view buffer);
-		bool save(std::wstring_view path);
+		bool load(const std::vector<MyVec3>& points, const std::vector<T>& indices);
+		bool save(const std::wstring& path);
 	private:
+		void clear();
 		void extractChunk(const char* data, size_t size);
 	private:
 		Header m_header;
+		std::vector<BufferView> m_bufferViews;
 		std::vector<Chunk> m_chunks;
 	};
 }
