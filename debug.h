d08c03b66e589861f90d086f5027a20db9848ceb
馬迪 <di.ma@bonsmile.com>
2023-08-13 23:52:40 +0800

    fix 更新到 fmt-v10.1.0 后的编译错误

--- debug.h
+++ debug.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "defines.h"
 #if defined(_WIN32)
@@ -12,10 +12,20 @@
 
 //#endif
 namespace lxd {
+	/// <summary>
+	/// https://stackoverflow.com/a/68675384/853569
+	/// fmtlib 要求第一个参数必须是编译期确定的，有两种解决方法：
+	/// 
+	/// fmt::format(fmt::runtime(fmt), std::forward<Args>(args)...)
+	/// 
+	/// std::vformat(fmt, fmt::make_format_args(std::forward<Args>(args)...))
+	/// 
+	/// 这里选择第二种，因为编译期的格式合法性检查很重要
+	/// </summary>
 	template <typename... Args>
 	DLL_PUBLIC void print(fmt::string_view fmt, Args&&... args) {
 #ifdef _WIN32
-		std::string output = fmt::vformat(fmt, fmt::make_format_args(std::forward<Args>(args)...));
+	    std::string output = fmt::vformat(fmt, fmt::make_format_args(args...));
 		OutputDebugStringA(output.c_str());
 #else
 		fmt::print(fmt::runtime(fmt), std::forward<Args>(args)...);
@@ -24,7 +34,7 @@ namespace lxd {
 #ifdef _WIN32
 	template <typename... Args>
 	DLL_PUBLIC void print(fmt::wstring_view fmt, Args&&... args) {
-		std::wstring output = fmt::vformat(fmt, fmt::make_wformat_args(std::forward<Args>(args)...));
+		std::wstring output = fmt::vformat(fmt, fmt::make_wformat_args(args...));
 		OutputDebugStringW(output.c_str());
 	}
 #endif
